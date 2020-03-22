4d056cfed6a94e72bdc32f5ad5427924eb26c429
Vítor <bsbribas66@gmail.com>
2020-03-21 22:32:10 -0300

    test nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'); // import Express and return a function
 const app = express();
 
 app.get('/', (req, res) => {  // (requisition, response)
-  res.send('Hello, Vítor')
+  res.send('Hello, Vitão')
 });
 
 app.listen(3001);
